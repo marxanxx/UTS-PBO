404a36da62ea7e8d10f9fd96f8f8d43fc82177ba
MARIANI <117056934+marxanxx@users.noreply.github.com>
2022-11-01 19:28:58 +0800

    PBO Awal

---
+++ LatihanPBO_awal/src/PackageLatihanPBO/Biodata.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PackageLatihanPBO;
+
+/**
+ *
+ * @author User
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ini contoh komentar
+        /*
+        ini 
+        adalah
+        contoh 
+        komen
+        */
+        System.out.println("Nama    : Mariani");
+        System.out.println("NIM     : 220140024");
+        System.out.println("Alamat  : Desa Setiap");
+    }
+    
+}
---
+++ LatihanPBO_awal/src/PackageLatihanPBO/Salam.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PackageLatihanPBO;
+
+/**
+ *
+ * @author User
+ */
+public class Salam {
+    public static void main(String[] args) {
+        System.out.println("Nama : Mariani");
+        System.out.println("NIM : 220140024");
+        System.out.println("Alamat : Desa Setiap");
+    }
+ 
+}
